0b27cbd211182f8c54b00d231c94c7c0e71cb2ef
dboeckli <dboeckli@gmail.com>
2025-04-16 08:58:34 +0200

    create domain and repo

---
+++ src/test/java/ch/dboeckli/guru/jpa/creditcard/repository/mysql/CreditCardRepositoryIT.java
@@ -0,0 +1,36 @@
+package ch.dboeckli.guru.jpa.creditcard.repository.mysql;
+
+import ch.dboeckli.guru.jpa.creditcard.domain.CreditCard;
+import ch.dboeckli.guru.jpa.creditcard.repository.CreditCardRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ActiveProfiles("test_mysql")
+@DirtiesContext
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)  // to assure that it is not replaced with h2
+
+class CreditCardRepositoryIT {
+
+    @Autowired
+    CreditCardRepository creditCardRepository;
+
+    @Test
+    void testJpaTestSplice() {
+        long countBefore = creditCardRepository.count();
+
+        creditCardRepository.save(new CreditCard());
+
+        long countAfter = creditCardRepository.count();
+
+        assertEquals(0, countBefore);
+        assertEquals(1, countAfter);
+    }
+
+}
