3d197066f07bebf40ac99f2f0f13b840df590f89
dboeckli <dboeckli@gmail.com>
2025-04-16 13:38:35 +0200

    implement listener for cvv encryption

---
+++ src/main/java/ch/dboeckli/guru/jpa/creditcard/listener/PreInsertListener.java
@@ -0,0 +1,59 @@
+package ch.dboeckli.guru.jpa.creditcard.listener;
+
+import ch.dboeckli.guru.jpa.creditcard.domain.CreditCard;
+import ch.dboeckli.guru.jpa.creditcard.service.EncryptionService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.event.spi.PreInsertEvent;
+import org.hibernate.event.spi.PreInsertEventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class PreInsertListener implements PreInsertEventListener {
+
+    private final EncryptionService encryptionService;
+
+    @Override
+    public boolean onPreInsert(PreInsertEvent event) {
+        log.info("Pre-insert event for entity: {}", event.getEntity());
+        if (event.getEntity() instanceof CreditCard creditCard) {
+            encryptCvv(creditCard, event);
+        }
+        return false;
+    }
+
+    private void encryptCvv(CreditCard creditCard, PreInsertEvent event) {
+        try {
+            String encryptedCvv = encryptionService.encrypt(creditCard.getCvv());
+
+            Object[] state = event.getState();
+            int cvvIndex = getCvvIndex(event);
+
+            if (cvvIndex != -1) {
+                state[cvvIndex] = encryptedCvv;
+                creditCard.setCvv(encryptedCvv);
+                log.info("Successfully encrypted CVV for credit card: {}", creditCard);
+            } else {
+                log.warn("CVV property not found for credit card: {}", creditCard);
+            }
+        } catch (Exception e) {
+            log.error("Error encrypting CVV for credit card: {}", creditCard, e);
+        }
+    }
+
+    private int getCvvIndex(PreInsertEvent event) {
+        return getPropertyIndex(event, "cvv");
+    }
+
+    private int getPropertyIndex(PreInsertEvent event, String propertyName) {
+        String[] propertyNames = event.getPersister().getPropertyNames();
+        for (int i = 0; i < propertyNames.length; i++) {
+            if (propertyNames[i].equals(propertyName)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
---
+++ src/main/java/ch/dboeckli/guru/jpa/creditcard/listener/PreUpdateListener.java
@@ -0,0 +1,59 @@
+package ch.dboeckli.guru.jpa.creditcard.listener;
+
+import ch.dboeckli.guru.jpa.creditcard.domain.CreditCard;
+import ch.dboeckli.guru.jpa.creditcard.service.EncryptionService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.event.spi.PreUpdateEvent;
+import org.hibernate.event.spi.PreUpdateEventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class PreUpdateListener implements PreUpdateEventListener {
+
+    private final EncryptionService encryptionService;
+
+    @Override
+    public boolean onPreUpdate(PreUpdateEvent event) {
+        log.info("Pre-update event for entity: {}", event.getEntity());
+
+        if (event.getEntity() instanceof CreditCard creditCard) {
+            encryptCvvIfChanged(creditCard, event);
+        }
+
+        return false;
+    }
+
+    private void encryptCvvIfChanged(CreditCard creditCard, PreUpdateEvent event) {
+        Object[] currentState = event.getState();
+        String[] propertyNames = event.getPersister().getPropertyNames();
+
+        int cvvIndex = getCvvIndex(propertyNames);
+        if (cvvIndex != -1) {
+            String currentCvv = (String) currentState[cvvIndex];
+            if (currentCvv != null && !currentCvv.equals(creditCard.getCvv())) {
+                try {
+                    String encryptedCvv = encryptionService.encrypt(creditCard.getCvv());
+                    creditCard.setCvv(encryptedCvv); // Update the credit card with the encrypted CVV
+                    currentState[cvvIndex] = encryptedCvv;
+                    log.info("Successfully encrypted updated CVV for credit card: {}", creditCard.getId());
+                } catch (Exception e) {
+                    log.error("Error encrypting updated CVV for credit card: {}", creditCard.getId(), e);
+                }
+            }
+        } else {
+            log.warn("CVV property not found for credit card: {}", creditCard.getId());
+        }
+    }
+
+    private int getCvvIndex(String[] propertyNames) {
+        for (int i = 0; i < propertyNames.length; i++) {
+            if ("cvv".equals(propertyNames[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
